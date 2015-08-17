6ad927220eb1e65083d7739d1db61fc91fe47401
Marco Muths <mamuz.de@gmail.com>
2015-08-17 17:42:28 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,4 +7,4 @@ include './vendor/autoload.php';
 $env = trim(file_get_contents('./ENV'));
 $configCache = './data/cache/config.php';
 
-Phapp\Application\Bootstrap::init($env, $env, $env === 'development' ? null : $configCache)->runApplicationOn($_SERVER);
+Phapp\Application\Bootstrap::init($env, $env === 'development' ? null : $configCache)->runApplicationOn($_SERVER);
